fae4b9a0febaf244ba4633c3979b2337779b26ad
jdiazrdgz <jdiazrdgz@gmail.com>
2017-04-10 21:19:39 -0500

    Nueva estructura de organización de vistas, se añade seccion de analisis y elemento del menu de analisis

--- DocumentacionAlbercaIPN/routes/index.js
+++ DocumentacionAlbercaIPN/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/inicio', function(req, res, next) {
+    res.render('inicio', { title: 'Contexto del Proyecto' });
+});
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('inicio', { title: 'Contexto del Proyecto' });
+});
+router.get('/analisis', function(req, res, next) {
+    res.render('analisis', { title: 'Analisis del Proyecto' });
 });
 
 module.exports = router;
